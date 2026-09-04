d402d9a44cf547435b86edd9532e02a4a9a5cf75
Thoha <2605674@sit.singaporetech.edu.sg>
2026-09-04 14:46:02 +0800

    Added Follower Growth tracker

--- hello.py
+++ hello.py
@@ -3,11 +3,30 @@ print("Welcome Here!")
 print("My first post!")
 print("======================")
 
-username = "cool_creator"
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
 bio = "Fun Blogger"
-followers = "100"
+category = input("Enter Content Category: ")
+followers = 100
 
+followers += 50
+print("Day 1: ", followers)
+
+followers += 20
+print("Day 2: ", followers)
+
+followers += 10
+print("Day 3: ", followers)
+
+print("\nInstagram Profile")
+print("======================")
 print("Username: ", username)
+print("Age: ", age)
 print("Bio: ", bio)
 print("Followers: ", followers)
+print("Category: ", category)
+
+if age>40 and category == "fun":
+    print("Fun has no boundaries, so enjoy no matter if you're " + str(age) + " or beyond!")
+
 
