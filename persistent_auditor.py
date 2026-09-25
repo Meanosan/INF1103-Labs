55853eef2464f21f25c32a99b09c04795fc73493
Maenosan <2605674@sit.singaporetech.edu.sg>
2026-09-25 14:54:46 +0800

    File read/write and append

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,3 +1,8 @@
+def load_inventory():
+     with open("inventory.txt", "w+") as file:
+          inventory = file.readlines()
+     return inventory
+
 def process_delivery(current_total,new_value):
           current_total = current_total + new_value
           print("Current Inventory: " + str(current_total))
@@ -10,15 +15,24 @@ def calculate_tax(amount):
 def generate_report(total_units, failed_attempts):
      print("Total Units Processed: " + str(total_units) + " Numnber of failed Entries: " +  str(failed_attempts) + ".")
 
+def save_inventory(inventory):
+     with open("inventory.txt", "w") as file:
+          file.writelines(inventory)
+
 def get_valid_input():
      failed_attempts = 0
      total = 0
+     transaction = load_inventory()
+     UID = 1000
      while True:
           stock = str(input("Enter Stock Quantity or 'Quit': "))
           if stock.isdigit() == True:
                if total < 500:
+                    UID += 1
+                    transaction.append(f"{UID}, 1{stock}\n")
                     total = process_delivery(total, int(stock))
                     calculate_tax(int(total))
+                    save_inventory(transaction)
                else:
                     print("Alert! Inventory exceeds 500!")
                continue
@@ -29,5 +43,5 @@ def get_valid_input():
           else:
                generate_report(total, failed_attempts)
                break
-                      
+
 get_valid_input()
